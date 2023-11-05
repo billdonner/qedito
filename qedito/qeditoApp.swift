92bf6d3fcffd1d784bac36cbb8292a6ce12a1018
bill donner <billdonner@gmail.com>
2023-11-05 14:50:10 -0600

    Initial Commit

---
+++ qedito/qeditoApp.swift
@@ -0,0 +1,32 @@
+//
+//  qeditoApp.swift
+//  qedito
+//
+//  Created by bill donner on 11/5/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct qeditoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
